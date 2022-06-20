0f907a7baa7c9e5df5a5977b57948c5ef7504803
miruy <kim-yurim@gim-yulim-ui-MacBookAir.local>
2022-06-20 15:47:15 +0900

    Gatsby site part3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
+    title: "My First Gatsby Site",
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
